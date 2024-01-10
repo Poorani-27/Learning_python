429eb77485fc941dd3a49fc1b07545ea97a6109c
Poorani <tpoorani2002@gmail.com>
2024-01-10 23:05:18 +0530

    Day 1 files: Python script, README, and image

---
+++ Day1/first_program.py
@@ -0,0 +1,5 @@
+#use Print("____") .Whatever you write inside the double quotes ,it will be printed .
+#we can also use single Quote print('________')
+
+print ("Hello World !")
+print('Hello World !')
