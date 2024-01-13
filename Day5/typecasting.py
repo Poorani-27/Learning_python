11bf5136ac47f5382798bb9551d5078c01038938
Poorani <tpoorani2002@gmail.com>
2024-01-14 00:19:04 +0530

    Typecasting , string fUnction and manipulation

---
+++ Day5/typecasting.py
@@ -0,0 +1,32 @@
+# Typecasting a string to an integer
+string_number = "42"
+integer_number = int(string_number)
+# Display the result
+print(f"String: {string_number}, Integer: {integer_number}")
+
+
+# Typecasting an integer to a string
+integer_value = 42
+string_value = str(integer_value)
+# Display the result
+print(f"Integer: {integer_value}, String: {string_value}")
+
+# Typecasting a string to a float
+string_float = "3.14"
+float_number = float(string_float)
+# Display the result
+print(f"String: {string_float}, Float: {float_number}")
+
+# Typecasting a float to an integer
+float_number = 3.14
+integer_number = int(float_number)
+# Display the result
+print(f"Float: {float_number}, Integer: {integer_number}")
+
+# Typecasting an integer to a boolean
+integer_value = 0
+boolean_value = bool(integer_value)
+# Display the result
+print(f"Integer: {integer_value}, Boolean: {boolean_value}")
+
+
