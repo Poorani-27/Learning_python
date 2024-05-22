7f8173e6e64d35660e015ecde199f9a07d0da289
Poorani <tpoorani2002@gmail.com>
2024-05-22 18:42:09 +0530

    added string literals.py

---
+++ python/01.string_literals.py
@@ -0,0 +1,10 @@
+#string literals
+
+print("\\n -- new line")
+print("\\t -- tab")
+print("\\\ -- backslash")
+single_quoted_string = 'It\'s a single quote '
+print("single quotes in single_quoted_string\t", single_quoted_string)
+double_quoted_string = "she said, \"hello\""
+print("single quotes in single_quoted_string\t", double_quoted_string)
+
